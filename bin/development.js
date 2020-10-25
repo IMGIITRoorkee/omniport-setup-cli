dc2d8699a6000b4562acc202a5deb00a000434e3
Gagan Sharma <gagan@gagan-HP-Pavilion-Gaming-Laptop-15-dk0xxx>
2020-10-25 15:36:46 +0530

    error handling and docker-compose configuraion

--- bin/development.js
+++ bin/development.js
@@ -1,122 +1,142 @@
 const log = console.log
 const fs = require('fs')
+const yaml = require('js-yaml')
 const cp = require('child_process')
 const inquirer = require("inquirer")
 const chalk = require('chalk')
+const dotenv = require('dotenv')
 module.exports = {
     SetUp: async () => {
-        let cloneYN = await inquirer
-            .prompt([{
-                name: "cloneornot",
-                type: "confirm",
-                message: "Do you want to clone Omniport Frontend and Backend Repository:",
-            }])
-
-        let Nfolder = cloneYN.cloneornot;
-        let answer;
-        if(!Nfolder){
-            answer = await inquirer
-            .prompt([{
-                name: "cloneHere",
-                type: "input",
-                message: "Enter the path of Omniport-Docker folder:",
-            }])
+        let opt = {
+            shell: true, 
+            cwd: './omniport-docker/', 
+            stdio: 'inherit'
         }
-        while(Nfolder){
-            answer = await inquirer
-            .prompt([{
-                name: "cloneHere",
-                type: "input",
-                message: "Where would you like to clone Omniport-Docker Repository:",
-            }])
-            Nfolder = await !fs.existsSync(answer.cloneHere)
-            if(Nfolder){
-                log('Enter a folder path.')
-                continue
+        let CloneError = true;
+        while (CloneError) {
+            let cloneYN = await inquirer
+                .prompt([{
+                    name: "cloneornot",
+                    type: "confirm",
+                    message: "Do you want to clone Omniport Frontend and Backend Repository:",
+                }])
+
+            CloneError = cloneYN.cloneornot;
+            if(CloneError){ 
+                try{
+                    cp.execSync("git clone https://github.com/IMGIITRoorkee/omniport-docker.git")
+                    cp.spawnSync('./scripts/clone/everything.sh', [], opt)         
+                    CloneError = false
+                }catch{
+                    log(chalk.keyword('red')('Check your Internet Connection or permissions of the folder'))
+                }
             }
+            
             try{
-                cp.execSync("git clone https://github.com/IMGIITRoorkee/omniport-docker.git", {cwd: answer.cloneHere})
-                cp.spawnSync('./scripts/clone/everything.sh', [], {cwd: answer.cloneHere+'/omniport-docker/', stdio: 'inherit'})         
+                cp.execSync('cd ./omniport-docker/')
             }catch{
-                log(chalk.keyword('red')('Check your Internet Connection or permissions of the folder'))
-                Nfolder = true
+                CloneError = true
+                log(chalk.keyword('red')('You need to first clone Omniport-Docker Repository'))
+            }
+
+            if(!CloneError){
+                let doc = yaml.safeLoad(fs.readFileSync('./omniport-docker/docker-compose.yml'))
+                let base = yaml.safeLoad(fs.readFileSync('./omniport-docker/codebase/omniport-backend/configuration/base_stencil.yml'))
+                log('Building images for Omniport...')
+                let build = await inquirer
+                    .prompt([{
+                        name: 'ifbuild',
+                        type: 'confirm',
+                        message: 'Build Omniport-django images?'
+                    }])
+                if(build.ifbuild){
+                    cp.spawnSync('./scripts/build/django.sh', [],opt)
+                }
+                build = await inquirer
+                    .prompt([{
+                        name: 'ifbuild',
+                        type: 'confirm',
+                        message: 'Build Omniport-react images?'
+                    }])
+                if(build.ifbuild){
+                    cp.spawnSync('./scripts/build/react.sh', [], opt)
+                }
+                build = await inquirer
+                    .prompt([{
+                        name: 'ifbuild',
+                        type: 'confirm',
+                        message: 'Build Omniport-nginx images?'
+                    }])
+                if(build.ifbuild){
+                    cp.spawnSync('./scripts/build/nginx.sh', [], opt)
+                }
+                build = await inquirer
+                    .prompt([{
+                        name: 'ifbuild',
+                        type: 'confirm',
+                        message: 'Build Omniport-memchaced images?'
+                    }])
+                if(build.ifbuild){
+                    cp.spawnSync('./scripts/build/memcached.sh', [], opt)
+                }
+                build = await inquirer
+                    .prompt([{
+                        name: 'ifbuild',
+                        type: 'confirm',
+                        message: 'Build Omniport-rabbitmq images?'
+                    }])
+                if(build.ifbuild){
+                    cp.spawnSync('./scripts/build/rabbitmq.sh', [], opt)
+                    let msgenv = dotenv.config({path: './omniport-docker/rabbitmq/message_broker.env'})
+                    doc.services['message-broker'].user = msgenv.parsed.RABBITMQ_DEFAULT_USER
+                    fs.writeFile('./omniport-docker/docker-compose.yml', yaml.safeDump(doc), (err) => {
+                        if (err) {
+                            log(chalk.keyword('red')('Some error ocurred.'))
+                        }
+                    })
+                }
+                build = await inquirer
+                    .prompt([{
+                        name: 'ifbuild',
+                        type: 'confirm',
+                        message: 'Build Omniport-postgres images?'
+                    }])
+                if(build.ifbuild){
+                    cp.spawnSync('./scripts/build/postgres.sh', [], opt)
+                    let dbenv = dotenv.config({path: './omniport-docker/postgres/database.env'})
+                    doc.services.database.user = dbenv.parsed.POSTGRES_USER
+                    fs.writeFile('./omniport-docker/docker-compose.yml', yaml.safeDump(doc), (err) => {
+                        if (err) {
+                            log(chalk.keyword('red')('Some error ocurred.'))
+                        }
+                    })
+                }
+                build = await inquirer
+                    .prompt([{
+                        name: 'ifbuild',
+                        type: 'confirm',
+                        message: 'Build Omniport-redis images?'
+                    }])
+                if(build.ifbuild){
+                    cp.spawnSync('./scripts/build/redis.sh', [], opt)
+                }
+                build = await inquirer
+                    .prompt([{
+                        name: 'ifbuild',
+                        type: 'confirm',
+                        message: 'Set up Omniport services?'
+                    }])
+                if(build.ifbuild){
+                    try{
+                        
+                        cp.execSync('./scripts/start/development.sh', { cwd: './omniport-docker/',stdio: 'inherit'})
+                        log(chalk.keyword('orange')('Congrats!'),'Omniport is all set and ready to roll.')
+                    }catch{
+                        log(chalk.keyword('red')('Omniport is not fully Setup.'))
+                    }
+                }
+                log('Refer to official docs to learn more about Omniport. https://omniport.readthedocs.io/')
             }
         }
-
-        log('Building images for Omniport.')
-        let build = await inquirer
-            .prompt([{
-                name: 'ifbuild',
-                type: 'confirm',
-                message: 'Build Omniport-django images?'
-            }])
-        if(build.ifbuild){
-            cp.spawnSync('./scripts/build/django.sh', [], {cwd: answer.cloneHere+'/omniport-docker/', stdio: 'inherit'})
-        }
-        build = await inquirer
-            .prompt([{
-                name: 'ifbuild',
-                type: 'confirm',
-                message: 'Build Omniport-react images?'
-            }])
-        if(build.ifbuild){
-            cp.spawnSync('./scripts/build/react.sh', [], {cwd: answer.cloneHere+'/omniport-docker/', stdio: 'inherit'})
-        }
-        /*build = await inquirer
-            .prompt([{
-                name: 'ifbuild',
-                type: 'confirm',
-                message: 'Build Omniport-nginx images?'
-            }])
-        if(build.ifbuild){
-            cp.spawnSync('./scripts/build/nginx.sh', [], {cwd: answer.cloneHere+'/omniport-docker/', stdio: 'inherit'})
-        }
-        build = await inquirer
-            .prompt([{
-                name: 'ifbuild',
-                type: 'confirm',
-                message: 'Build Omniport-memchaced images?'
-            }])
-        if(build.ifbuild){
-            cp.spawnSync('./scripts/build/memcached.sh', [], {cwd: answer.cloneHere+'/omniport-docker/', stdio: 'inherit'})
-        }*/
-        build = await inquirer
-            .prompt([{
-                name: 'ifbuild',
-                type: 'confirm',
-                message: 'Build Omniport-rabbitmq images?'
-            }])
-        if(build.ifbuild){
-            cp.spawnSync('./scripts/build/rabbitmq.sh', [], {cwd: answer.cloneHere+'/omniport-docker/', stdio: 'inherit'})
-        }
-        build = await inquirer
-            .prompt([{
-                name: 'ifbuild',
-                type: 'confirm',
-                message: 'Build Omniport-postgres images?'
-            }])
-        if(build.ifbuild){
-            cp.spawnSync('./scripts/build/postgres.sh', [], {cwd: answer.cloneHere+'/omniport-docker/', stdio: 'inherit'})
-        }
-        build = await inquirer
-            .prompt([{
-                name: 'ifbuild',
-                type: 'confirm',
-                message: 'Build Omniport-redis images?'
-            }])
-        if(build.ifbuild){
-            cp.spawnSync('./scripts/build/redis.sh', [], {cwd: answer.cloneHere+'/omniport-docker/', stdio: 'inherit'})
-        }
-        build = await inquirer
-            .prompt([{
-                name: 'ifbuild',
-                type: 'confirm',
-                message: 'Set up Omniport services?'
-            }])
-        if(build.ifbuild){
-            cp.spawnSync('./scripts/start/development.sh', [], {cwd: answer.cloneHere+'/omniport-docker/', stdio: 'inherit'})
-            log(chalk.keyword('orange')('Congrats!'),'Omniport is all set and ready to roll.')
-        }
-        log('Refer to official docs to learn more about Omniport. https://omniport.readthedocs.io/')
     }
 }
--- bin/index.js
+++ bin/index.js
@@ -3,38 +3,51 @@ const chalk = require('chalk')
 //const clear = require('clear')
 const figlet = require('figlet')
 const yargs = require("yargs")
+const cp = require('child_process')
 const inquirer = require("inquirer")
 const D = require("./development.js")
 const P = require("./production.js")
 const log = console.log
+const arg = process.argv
 const options = yargs
 .usage('Usage: $0 <command> [options]')
 .command('start', 'Setup Omniport services and start the servers ')
-.help('h')
+.command('setup', 'Setup Omniport Docker')
 .alias('h', 'help')
 .alias('v', 'version')
 .argv
 
 log(
   chalk.keyword('blue').bold(
-    figlet.textSync('Omniport', { horizontalLayout: 'fitted' })
+    figlet.textSync('Omniport', { font:'Colossal', horizontalLayout: 'fitted' })
   )
 )
-
-inquirer
-  .prompt([
-    {
-      name: "choice",
-      type: "list",
-      message: "Let\'s get you started with Omniport. Make sure you have Docker and Docker-Compose installed and daemon is running. Choose what fits you the best:",
-      choices: ["Development Setup", "Production Setup"],
-    },
-  ])
-  .then((answer) => {
-    if(answer.choice == "Development Setup"){
-      D.SetUp()
-    }
-    else{
-      P.SetUp()
-    }
-  })
+if(arg[2]==undefined){
+  log('Omniport is a portal for educational institutes, designed from the ground up to be extensible, customizable, performant and powerful. Omniport also comes with a powerful collection of apps and is simple enough for you to be able to write your own.\nSee \'omniport --help\' for using this tool.')
+}
+else if(arg[2]=='setup'&& arg[3]==undefined){
+  //log('setup') 
+  inquirer
+    .prompt([
+      {
+        name: "choice",
+        type: "list",
+        message: "Let\'s get you started with Omniport. Make sure you\n \u2022 Are in the folder where you want to clone/ have cloned Omniport-Docker Repository\n \u2022 Have Docker and Docker-Compose installed\n \u2022 Docker Daemon is running\n You can use Ctrl+C to exit anytime. Choose what fits you the best:",
+        choices: ["Development Setup", "Production Setup"],
+      },
+    ])
+    .then((answer) => {
+      if(answer.choice == "Development Setup"){
+        D.SetUp()
+      }
+      else{
+        P.SetUp()
+      }
+    })
+}
+else if(arg[2]=='start' && arg[3]==undefined){
+  log('start')
+}
+else{
+  cp.spawn('omniport', ['-h'], { stdio: 'inherit'})
+}
